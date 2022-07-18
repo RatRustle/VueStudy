a3dfa3f5fe3b7a0c7af6227901fb4640076a2c79
rot <rot@qq.com>
2022-07-18 17:19:55 +0800

    vuex学习

---
+++ vue_study/vuex_study/vue.config.js
@@ -0,0 +1,13 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  pages: {
+    index: {
+      //入口
+      entry: 'src/main.js'
+    }
+  },
+  lintOnSave: false,//关闭语法检查
+}
